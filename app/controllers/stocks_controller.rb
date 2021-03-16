1782aeae909a6e385748308dbb49061e97874fde
Carlos Villa <cvillad@uninorte.edu.co>
2021-03-16 14:48:28 -0500

    add use ajax for search stocks

--- app/controllers/stocks_controller.rb
+++ app/controllers/stocks_controller.rb
@@ -3,14 +3,20 @@ class StocksController < ApplicationController
     if params[:stock].present?
       @stock = Stock.new_lookup(params[:stock])
       if @stock
-        render "users/my_portfolio"
+        respond_to do |format|
+          format.js { render partial: "users/result" }
+        end
       else
-        flash[:alert] = "Symbol not found"
-        redirect_to my_portfolio_path
+        respond_to do |format|
+          flash.now[:alert] = "Symbol not found"
+          format.js { render partial: "users/result" }
+        end
       end
     else
-      flash[:alert] = "Enter a symbol to search"
-      redirect_to my_portfolio_path
+      respond_to do |format|
+        flash.now[:alert] = "Enter a symbol to search"
+        format.js { render partial: "users/result" }
+      end
     end
   end
 end
