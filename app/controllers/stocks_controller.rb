50254a5bbea9a561a7aa12bae6c90ca7808c9a95
Carlos Villa <cvillad@uninorte.edu.co>
2021-03-16 14:04:32 -0500

    add stocks search

---
+++ app/controllers/stocks_controller.rb
@@ -0,0 +1,16 @@
+class StocksController < ApplicationController
+  def search
+    if params[:stock].present?
+      @stock = Stock.new_lookup(params[:stock])
+      if @stock
+        render "users/my_portfolio"
+      else
+        flash[:alert] = "Symbol not found"
+        redirect_to my_portfolio_path
+      end
+    else
+      flash[:alert] = "Enter a symbol to search"
+      redirect_to my_portfolio_path
+    end
+  end
+end
