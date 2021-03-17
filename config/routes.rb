433b1fedd1a0238ca24b276af6ff9c29e5fd9eff
Carlos Villa <cvillad@uninorte.edu.co>
2021-03-17 09:54:25 -0500

    add friends list

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,6 @@ Rails.application.routes.draw do
   root to: "pages#index"
   devise_for :users
   get "my_portfolio", to: "users#my_portfolio"
+  get "friends", to: "users#friends"
   get "search_stock", to: "stocks#search"
 end
