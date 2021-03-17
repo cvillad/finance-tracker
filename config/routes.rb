4de4dde91fdfb2c9e6f1a6196f67c187153a072c
Carlos Villa <cvillad@uninorte.edu.co>
2021-03-17 14:25:18 -0500

    add show user

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   resources :friendships, only: [:create, :destroy]
   root to: "pages#index"
   devise_for :users
+  resources :users, only: [:show]
   get "my_portfolio", to: "users#my_portfolio"
   get "my_friends", to: "users#my_friends"
   get "search_friend", to: "users#search"
